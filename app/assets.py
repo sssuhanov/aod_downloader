f95af07e5bfee2aabe689292712b8977b19d9b48
sss <ssskhnv@gmail.com>
2024-03-31 20:52:11 -0700

    unstructured data asset, metadata

--- app/assets.py
+++ app/assets.py
@@ -1,10 +1,14 @@
 import json
 import os
+import base64
+from io import BytesIO
 
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
 
-from dagster import asset, Definitions # import the `dagster` library
+from dagster import AssetExecutionContext, MetadataValue, asset, MaterializeResult, Definitions
+
 
 @asset # add the asset decorator to tell Dagster this is an asset
 def topstory_ids() -> None:
@@ -16,8 +20,8 @@ def topstory_ids() -> None:
         json.dump(top_new_story_ids, f)
 
 
-@asset(deps=[topstory_ids])  # this asset is dependent on topstory_ids
-def topstories() -> None:
+@asset(deps=[topstory_ids])
+def topstories(context: AssetExecutionContext) -> MaterializeResult:
     with open("data/topstory_ids.json", "r") as f:
         topstory_ids = json.load(f)
 
@@ -29,12 +33,49 @@ def topstories() -> None:
         results.append(item)
 
         if len(results) % 20 == 0:
-            print(f"Got {len(results)} items so far.")
+            context.log.info(f"Got {len(results)} items so far.")
 
     df = pd.DataFrame(results)
     df.to_csv("data/topstories.csv")
 
+    return MaterializeResult(
+        metadata={
+            "num_records": len(df),  # Metadata can be any key-value pair
+            "preview": MetadataValue.md(df.head().to_markdown()),
+            # The `MetadataValue` class has useful static methods to build Metadata
+        }
+    )
+
+@asset(deps=[topstories])
+def most_frequent_words() -> None:
+    stopwords = ["a", "the", "an", "of", "to", "in", "for", "and", "with", "on", "is"]
+
+    topstories = pd.read_csv("data/topstories.csv")
+
+    # loop through the titles and count the frequency of each word
+    word_counts = {}
+    for raw_title in topstories["title"]:
+        title = raw_title.lower()
+        for word in title.split():
+            cleaned_word = word.strip(".,-!?:;()[]'\"-")
+            if cleaned_word not in stopwords and len(cleaned_word) > 0:
+                word_counts[cleaned_word] = word_counts.get(cleaned_word, 0) + 1
+
+    # Get the top 25 most frequent words
+    top_words = {
+        pair[0]: pair[1]
+        for pair in sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:25]
+    }
+
+    with open("data/most_frequent_words.json", "w") as f:
+        json.dump(top_words, f)
+
+all_assets = [
+    topstory_ids,
+    topstories,
+    most_frequent_words
+]
 
 defs = Definitions(
-    assets=[topstory_ids, topstories],
+    assets=all_assets,
 )
