fed1878b8097a1819d114e35930122d447a798de
sss <ssskhnv@gmail.com>
2024-03-31 19:32:13 -0700

    app/data to ignore and new assets from tutorial

---
+++ app/assets.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+import pandas as pd
+import requests
+
+from dagster import asset, Definitions # import the `dagster` library
+
+@asset # add the asset decorator to tell Dagster this is an asset
+def topstory_ids() -> None:
+    newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    top_new_story_ids = requests.get(newstories_url).json()[:100]
+
+    os.makedirs("data", exist_ok=True)
+    with open("data/topstory_ids.json", "w") as f:
+        json.dump(top_new_story_ids, f)
+
+
+@asset(deps=[topstory_ids])  # this asset is dependent on topstory_ids
+def topstories() -> None:
+    with open("data/topstory_ids.json", "r") as f:
+        topstory_ids = json.load(f)
+
+    results = []
+    for item_id in topstory_ids:
+        item = requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
+        ).json()
+        results.append(item)
+
+        if len(results) % 20 == 0:
+            print(f"Got {len(results)} items so far.")
+
+    df = pd.DataFrame(results)
+    df.to_csv("data/topstories.csv")
+
+
+defs = Definitions(
+    assets=[topstory_ids, topstories],
+)
