22819ec099ad4eee04b18be197925b444c099723
sss <ssskhnv@gmail.com>
2024-05-02 19:16:29 -0700

    add average price and quantity diffs

--- app/dbt/models/marts/market_volume.sql
+++ app/dbt/models/marts/market_volume.sql
@@ -6,7 +6,13 @@ select
     max(q."timestamp") as "timestamp",
 	avg(q.avg_price)::numeric(20,2) as avg_price,
 	avg(q.item_count)::numeric(20,2) as avg_count,
-	(avg(q.avg_price)::numeric(20,2) * avg(q.item_count)::numeric(20,2))::numeric(20,2) as volume
+	(avg(q.avg_price)::numeric(20,2) * avg(q.item_count)::numeric(20,2))::numeric(20,2) as volume,
+	min(q.avg_price)::numeric(20,2) as min_avg_price,
+	max(q.avg_price)::numeric(20,2) as max_avg_price,
+	max(q.avg_price)::numeric(20,2) / min(q.avg_price)::numeric(20,2) as avg_price_diff,
+	min(q.item_count)::numeric(20,2) as min_item_count,
+	max(q.item_count)::numeric(20,2) as max_item_count,
+	max(q.item_count)::numeric(20,2) / min(q.item_count)::numeric(20,2) as avg_count_diff
 from {{ ref("stg_aod__history_2") }} q
 group by
 	q.item_id,
--- app/dbt/models/marts/total_volume.sql
+++ app/dbt/models/marts/total_volume.sql
@@ -1,13 +1,17 @@
 select
 	mv.item_id,
 	mv.item_name item_name,
-	mv."location" ,
+	mv."location",
 	min(mv."timestamp") min_date,
-	sum(mv.volume) as volume
+	sum(mv.volume) as volume,
+	mv.avg_price_diff::numeric(20,2) as avg_price_diff,
+	mv.avg_count_diff::numeric(20,2) as avg_count_diff
 from {{ ref("market_volume")}} mv
 group by 
 	mv.item_id,
 	mv.item_name,
-	mv."location"
+	mv."location",
+	mv.avg_price_diff,
+	mv.avg_count_diff
 order by
     volume desc
