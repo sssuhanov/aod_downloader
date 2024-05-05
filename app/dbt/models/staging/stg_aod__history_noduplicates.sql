6e9bf9cb73744476f41a4ca8fa2b04b7e01d44af
sss <ssskhnv@gmail.com>
2024-05-04 21:06:47 -0700

    delete duplicates from history

---
+++ app/dbt/models/staging/stg_aod__history_noduplicates.sql
@@ -0,0 +1,22 @@
+WITH RankedData AS (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY sah.item_id, sah.item_name, sah.quality, sah."location", sah."timestamp"
+           ORDER BY sah.created_at DESC) AS rn
+    FROM {{ ref("stg_aod__history_2")}} sah
+)
+select
+	rd.item_id,
+	rd.created_at,
+	rd.item_name,
+	rd.quality,
+	rd."location",
+	rd.avg_price,
+	rd."timestamp",
+	rd.item_count
+FROM RankedData rd
+WHERE rn = 1
+order by
+	rd.item_id,
+	rd.quality,
+	rd."location",
+	rd."timestamp"
