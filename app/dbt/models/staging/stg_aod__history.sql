9c37309dd597c963c8ac5e983ca951571ffe2cf3
sss <ssskhnv@gmail.com>
2024-05-23 21:17:17 -0700

    fix data types

--- app/dbt/models/staging/stg_aod__history.sql
+++ app/dbt/models/staging/stg_aod__history.sql
@@ -1,8 +1,8 @@
 select
-	h.id as id,
-	h.created_at as created_at,
-	jsonb_array_elements(h."data")->'item_id'::text as item_id,
-	jsonb_array_elements(h."data")->'quality'::text as quality,
-	jsonb_array_elements(h."data")->'location'::text as location,
-	jsonb_array_elements(h."data")->'data' as data
+	(h.id)::text as id,
+	(h.created_at)::timestamp as created_at,
+	(jsonb_array_elements(h."data")->'item_id')::text as item_id,
+	(jsonb_array_elements(h."data")->'quality')::text as quality,
+	(jsonb_array_elements(h."data")->'location')::text as location,
+	(jsonb_array_elements(h."data")->'data')::jsonb as data
 from {{ source('aod', 'history') }} h
--- app/dbt/models/staging/stg_aod__items_names.sql
+++ app/dbt/models/staging/stg_aod__items_names.sql
@@ -1,11 +1,11 @@
 select
-	jsonb_array_elements(i."data")->'Index'::text as id,
-	jsonb_array_elements(i."data")->'UniqueName'::text as unique_name,
-	jsonb_array_elements(i."data")->'LocalizationNameVariable'::text as local_name_var,
-	jsonb_array_elements(i."data")->'LocalizationDescriptionVariable'::text as local_desc_var,
-	jsonb_array_elements(i."data")->'LocalizedNames' as local_names,
-	jsonb_array_elements(i."data")->'LocalizedDescriptions' as local_desc,
-	jsonb_array_elements(i."data")->'LocalizedNames'->'EN-US'::text as en_us
+	(jsonb_array_elements(i."data")->'Index')::text as id,
+	(jsonb_array_elements(i."data")->'UniqueName')::text as unique_name,
+	(jsonb_array_elements(i."data")->'LocalizationNameVariable')::text as local_name_var,
+	(jsonb_array_elements(i."data")->'LocalizationDescriptionVariable')::text as local_desc_var,
+	(jsonb_array_elements(i."data")->'LocalizedNames')::jsonb as local_names,
+	(jsonb_array_elements(i."data")->'LocalizedDescriptions')::jsonb as local_desc,
+	(jsonb_array_elements(i."data")->'LocalizedNames'->'EN-US')::text as en_us
 from {{ source('aod', 'items_names')}} i 
 where 
 	created_at = (select max(created_at) from {{ source('aod', 'items_names')}})
