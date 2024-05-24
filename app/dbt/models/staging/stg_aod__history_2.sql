10a12c44f99e522838b100a34dbff60be9da259e
sss <ssskhnv@gmail.com>
2024-05-23 21:41:32 -0700

    fix data types

--- app/dbt/models/staging/stg_aod__history_2.sql
+++ app/dbt/models/staging/stg_aod__history_2.sql
@@ -1,18 +1,18 @@
 select
-	(h.item_id)::text item_id,
-	h.created_at::text::timestamp created_at,
-	i."en_us"::text as item_name,
-	i."tier"::text as item_tier,
-	i."enchantment"::text as item_enchantment,
-	(h.quality)::numeric quality,
-	(h."location")::text "location",
-	(jsonb_array_elements(h."data")->'avg_price')::numeric avg_price,
-	(jsonb_array_elements(h."data")->'timestamp')::text::timestamp "timestamp",
-	(jsonb_array_elements(h."data")->'item_count')::numeric item_count
+	h.item_id item_id,
+	h.created_at created_at,
+	i."en_us" as item_name,
+	i."tier" as item_tier,
+	i."enchantment" as item_enchantment,
+	(h.quality) quality,
+	(h."location") "location",
+	(jsonb_array_elements(h."data")->>'avg_price')::numeric avg_price,
+	(jsonb_array_elements(h."data")->>'timestamp')::text::timestamp "timestamp",
+	(jsonb_array_elements(h."data")->>'item_count')::numeric item_count
 from {{ ref("stg_aod__history")}} h
 left join {{ ref("stg_aod__items_names_plus")}} i on h.item_id = i."unique_name"
 order by
-	(h.item_id)::text,
-	(h.quality)::numeric,
-	(h."location")::text,
-	h.created_at::text::timestamp
+	h.item_id,
+	h.quality,
+	h."location",
+	h.created_at
